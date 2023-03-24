cf7d5de7b7baa908dca5293b67c7aa0cdf973e5c
MADElinessss <jyseen@naver.com>
2023-03-24 20:46:08 +0900

    Initial Commit

---
+++ Memo/MemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by 신정연 on 2023/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
